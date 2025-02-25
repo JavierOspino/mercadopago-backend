693782dfefe6ace80cf7c8eee1e933f826ec14a1
JavierOspino <javierospinoac@unimagdalena.edu.co>
2025-02-25 14:58:02 -0500

    Agregar protección de roles en el controlador de usuarios

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -2,6 +2,9 @@ import { Controller, Get, UseGuards, Request, Post, Body, Param, Patch } from '@
 import { AuthGuard } from '@nestjs/passport';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UserService } from './user.service';
+import { Roles } from '../auth/roles/roles.decorator';
+import { UserRole } from './user.entity';
+import { RolesGuard } from '../auth/roles/roles.guard';
 
 @Controller('user')
 export class UserController {
@@ -24,7 +27,8 @@ export class UserController {
   }
 
   @Get()
-  //@UseGuards(AuthGuard('jwt')) 
+  @Roles(UserRole.ADMIN)
+  @UseGuards(AuthGuard('jwt'), RolesGuard) 
   async findAll() {
     return this.userService.findAll();
   }
