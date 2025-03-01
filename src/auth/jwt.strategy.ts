73d90325a84a44d7ca412dd75f9b93d9357d10e6
JavierOspino <javierospinoac@unimagdalena.edu.co>
2025-03-01 00:56:18 -0500

    Agregar validación de usuario en la estrategia JWT y manejo de excepciones

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,19 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
+import { User } from '../users/user.entity';
+import { UserService } from '../users/user.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(configService: ConfigService) {
+    constructor(configService: ConfigService, private readonly userService: UserService) {
         super({
         jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
         ignoreExpiration: false,
-        secretOrKey: configService.get('JWT_SECRET') as string,
+        secretOrKey: configService.get<string>('JWT_SECRET') as string,
         });
     }
     
-    async validate(payload: any) {
-        return { userId: payload.sub, username: payload.username };
-    }
+    async validate(payload: { sub: string }): Promise<User> {
+        console.log('Payload recibido:', payload); // 👈 Verifica que `id` es un string
+        const user = await this.userService.findById(payload.sub);
+        console.log('Usuario encontrado:', user);
+      
+        if (!user) {
+          throw new UnauthorizedException('Usuario no encontrado');
+        }
+      
+        return user;
+      }
 }
